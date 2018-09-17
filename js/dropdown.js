b642a3e753f6fb8673b75751bde790a92f1f0f3d
cjang5 <hello@chrisjang.com>
2018-09-17 00:44:36 -0700

    Revised behavior for hamburger menu on mobile

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,9 +1,28 @@
-/* Ref: https://stackoverflow.com/questions/22015908/onclick-not-working-on-mobile-touch/22015946#22015946 */
+// Ref: https://gist.github.com/AndrewBarba/5477454
+jQuery.fn.touchClick = function(fnc)
+{
+    if (fnc) {
+        this.bind("touchend click",function(event){
+            event.stopPropagation();
+            event.preventDefault();
+            if(event.handled !== true) {
+                event.handled = true;
+                return fnc.call(this,event);
+            } else {
+                return false;
+            }
+        });
+    } else {
+        this.trigger("click");
+    }
+}
+
 const container = $('.dropdown-menu')
 const hamburger = $('.dropdown i')
-$(document).on('click touchstart', (e) => {
+$(document).touchClick((e) => {
   const hamburgerIsTarget = hamburger.is(e.target)
   if (hamburgerIsTarget) {
+    console.log("hamburger")
     container.toggle()
   } else if (!container.is(e.target) && !hamburgerIsTarget && container.has(e.target).length === 0) {
     container.hide()
