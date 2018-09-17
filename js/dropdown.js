15033131b7acd458cdc9291e6283e955d8329666
cjang5 <hello@chrisjang.com>
2018-09-17 00:19:42 -0700

    Fixing behavior on mobile

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,7 +1,7 @@
 /* Ref: https://stackoverflow.com/questions/22015908/onclick-not-working-on-mobile-touch/22015946#22015946 */
 const container = $('.dropdown-menu')
 const hamburger = $('.dropdown i')
-$(document).on('click touch', (e) => {
+$(document).on('click touchstart', (e) => {
   const hamburgerIsTarget = hamburger.is(e.target)
   if (hamburgerIsTarget) {
     container.toggle()
