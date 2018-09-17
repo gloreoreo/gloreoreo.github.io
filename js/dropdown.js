9c6dcd3a517e1a3fb2c4864f82165856b3470f30
cjang5 <chrismangggg@gmail.com>
2018-09-16 23:44:44 -0700

    Fixed hamburger behavior for mobile

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,10 +1,11 @@
-$(document).mouseup(function(e) {
-    const container = $('.dropdown-menu')
-    const hamburger = $('.dropdown i')
-
-    if (hamburger.is(e.target)) {
-        container.toggle()
-    } else if (!container.is(e.target) && !hamburger.is(e.target) && container.has(e.target).length === 0) {
-        container.hide()
-    }
+/* Ref: https://stackoverflow.com/questions/22015908/onclick-not-working-on-mobile-touch/22015946#22015946 */
+const container = $('.dropdown-menu')
+const hamburger = $('.dropdown i')
+$(document).on('click touchstart', (e) => {
+  const hamburgerIsTarget = hamburger.is(e.target)
+  if (hamburgerIsTarget) {
+    container.toggle()
+  } else if (!container.is(e.target) && !hamburgerIsTarget && container.has(e.target).length === 0) {
+    container.hide()
+  } 
 })
