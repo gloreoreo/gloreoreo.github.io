e15f646ae91e1f6ef37301c6807e42ee412e5f43
cjang5 <hello@chrisjang.com>
2018-09-17 01:25:39 -0700

    fuck it, only looking for touchstart

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,28 +1,8 @@
-// Ref: https://gist.github.com/AndrewBarba/5477454
-/*
-jQuery.fn.touchClick = function(fnc) {
-    if (fnc) {
-        this.bind("touchend click",function(event){
-            event.stopPropagation();
-            event.preventDefault();
-            if(event.handled !== true) {
-                event.handled = true;
-                return fnc.call(this,event);
-            } else {
-                return false;
-            }
-        });
-    } else {
-        this.trigger("click");
-    }
-}*/
-
 const container = $('.dropdown-menu')
 const hamburger = $('.dropdown i')
-$(document).click((e) => {
+$(document).on('touchend', (e) => {
   const hamburgerIsTarget = hamburger.is(e.target)
   if (hamburgerIsTarget) {
-    console.log("hamburger")
     container.toggle()
   } else if (!container.is(e.target) && !hamburgerIsTarget && container.has(e.target).length === 0) {
     container.hide()
