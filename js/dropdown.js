0f968e4e1f2fa1e0c4f828a08cc1e8f6928e7520
cjang5 <hello@chrisjang.com>
2018-09-17 00:02:02 -0700

    Trying touch instead of touchstart

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,10 +1,10 @@
 /* Ref: https://stackoverflow.com/questions/22015908/onclick-not-working-on-mobile-touch/22015946#22015946 */
 const container = $('.dropdown-menu')
 const hamburger = $('.dropdown i')
-$(document).on('click touchstart', (e) => {
+$(document).on('click touch', (e) => {
   e.stopPropagation()
   e.preventDefault()
-  
+
   const hamburgerIsTarget = hamburger.is(e.target)
   if (hamburgerIsTarget) {
     container.toggle()
